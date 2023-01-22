95190c1c4223e9030e457b16c4a38b9f99326f4e
frank <ffliu926@gmail.com>
2023-01-22 13:31:28 -0500

    ported transition into a module

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -3,30 +3,24 @@ import { GoogleOAuthProvider, GoogleLogin, googleLogout } from "@react-oauth/goo
 
 import "../../utilities.css";
 import "./Skeleton.css";
+import Transition from "../modules/Transition";
 import { Link } from "@reach/router";
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
 const GOOGLE_CLIENT_ID = "860650022251-idijj5uv8le61d1el5bbr5ts6atecan8.apps.googleusercontent.com";
 
 const Skeleton = ({ userId, handleLogin, handleLogout }) => {
-  const transition = document.querySelector(".transitions");
-  setTimeout(() => {
-    transition.classList.remove("active-transition");
-
-    //removes "active-transition" class from transition div, which sets its opacity to 0
-    //note: i removed "active-transition" class from the div because it bricked site after push
-  }, 300);
-
   return (
     <>
-      <div className="transitions"></div>
+      <Transition />
+      {/* delete this if site bricks to sign into google then put this back in*/}
 
       <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
         <div className="skeleton-title-over relative">
-          <h1 className="lightpink">EXAMPLE NAME</h1>
+          <h1 className="skeleton-lightpink">EXAMPLE NAME</h1>
         </div>
         <div className="skeleton-title-under relative">
-          <h1 className="darkpink">EXAMPLE NAME</h1>
+          <h1 className="skeleton-darkpink">EXAMPLE NAME</h1>
         </div>
 
         <div className="skeleton-google relative">
